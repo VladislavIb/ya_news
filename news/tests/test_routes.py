598fe4da3aff2f3fe76ebdc5ffa7ea5dd40be8fa
Vladislav Ibraev <vladislavibraev@gmail.com>
2024-08-24 12:36:42 +0500

    add docstrings to test_routes.py

--- news/tests/test_routes.py
+++ news/tests/test_routes.py
@@ -1,4 +1,4 @@
-# news/tests/test_routes.py
+"""Тестирование маршрутов."""
 from http import HTTPStatus
 
 from django.contrib.auth import get_user_model
@@ -12,9 +12,11 @@ User = get_user_model()
 
 
 class TestRoutes(TestCase):
+    """Тестирование маршрутов."""
 
     @classmethod
     def setUpTestData(cls):
+        """Создание новости для тестирования."""
         cls.news = News.objects.create(title='Заголовок', text='Текст')
         cls.author = User.objects.create(username='Лев Толстой')
         cls.reader = User.objects.create(username='Читатель простой')
@@ -25,6 +27,7 @@ class TestRoutes(TestCase):
         )
 
     def test_pages_availability(self):
+        """Проверка доступности страниц."""
         urls = (
             ('news:home', None),
             ('news:detail', (self.news.id,)),
@@ -39,6 +42,7 @@ class TestRoutes(TestCase):
                 self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_availability_for_comment_edit_and_delete(self):
+        """Проверка доступности редактирования и удаления комментария."""
         users_statuses = (
             (self.author, HTTPStatus.OK),
             (self.reader, HTTPStatus.NOT_FOUND),
@@ -48,13 +52,14 @@ class TestRoutes(TestCase):
             self.client.force_login(user)
             # Для каждой пары "пользователь - ожидаемый ответ"
             # перебираем имена тестируемых страниц:
-            for name in ('news:edit', 'news:delete'):  
-                with self.subTest(user=user, name=name):        
+            for name in ('news:edit', 'news:delete'):
+                with self.subTest(user=user, name=name):
                     url = reverse(name, args=(self.comment.id,))
                     response = self.client.get(url)
                     self.assertEqual(response.status_code, status)
 
     def test_redirect_for_anonymous_client(self):
+        """Проверка редиректа для анонимного пользователя."""
         login_url = reverse('users:login')
         for name in ('news:edit', 'news:delete'):
             with self.subTest(name=name):
